8563334377e1d961d8ec0db1bbb83073866fe870
Erika Rosero <20155033@tafe.wa.edu.au>
2025-09-04 13:53:00 +0800

    Update any_old_code.py

--- any_old_code.py
+++ any_old_code.py
@@ -1,10 +1,11 @@
 list_of_fruits= ["logan","rambutan","John Voigt", "McAfee"]
 for fruit in list_of_fruitscd
-TO list
+TO listmpo
 SESSION
 GIT ADD
-
+p,[l
 other line 
-and changes
+and changes,l[
 for see what
  happen
+,lp
